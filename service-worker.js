3ec3e78cf17e24224b4f3bb9d526a16b63238212
virbunny <virbunny@gmail.com>
2026-08-17 18:43:33 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-// 2026-08-10; 日本觀光列車｜旅印帖 v65 PWA
-const CACHE_NAME = 'jp-tourist-train-tabijirushi-v65-20260810-weekly-updates-cache';
+// 2026-08-17; 日本觀光列車｜旅印帖 v66 PWA
+const CACHE_NAME = 'jp-tourist-train-tabijirushi-v66-20260817-weekly-updates-cache';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './tourist-train-rabbit-icon-v65-180.png',
-  './tourist-train-rabbit-icon-v65-192.png',
-  './tourist-train-rabbit-icon-v65-512.png'
+  './tourist-train-rabbit-icon-v66-180.png',
+  './tourist-train-rabbit-icon-v66-192.png',
+  './tourist-train-rabbit-icon-v66-512.png'
 ];
 
 self.addEventListener('install', event => {
